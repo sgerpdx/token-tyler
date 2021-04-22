c1c244f98cb5e103fb015fa79d0f20ec3ed285be
Eion Nelson <ecnelson1@live.com>
2021-04-22 15:53:55 -0700

    rebuilding

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const TestContract = artifacts.require("TestContract");
-const Token = artifacts.require("Token");
-
-module.exports = function (deployer) {
-  deployer.deploy(TestContract);
-  deployer.deploy(Token);
-};
